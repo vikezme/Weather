a4b248a06689843f37ec065131c273eb01fd390d
kumar vikrant <vikezme@gmail.com>
2021-07-24 11:46:25 +0930

    adding file

--- weather.js
+++ weather.js
@@ -5,11 +5,37 @@ var desc= document.querySelector('.desc')
 var temp= document.querySelector('.temp')
 
 button.addEventListener('click', function (){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&
-  appid=771f4017d6098f3a2d947ab695755dca')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=771f4017d6098f3a2d947ab695755dca')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => this.displayWeather(data))
 
   .catch(err => alert("Wrong City Name"))
+})
 
+
+displayWeather: function(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  console.log(name, icon, description, temp, humidity, speed);
+  document.querySelector(".city").innerText = "Weather in" + name;
+  document.querySelector(".icon").src ="http://openweathermap.org/img/wn/" + icon + "01n@2x.png"
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".temp").innerText = temp + "°C";
+  document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+  document.querySelector(".wind").innerText = "Wind Speed:" + speed + "km/h";
+};
+
+  search: function (){
+    this.fetchWeather(document.querySelector(".inputValue").value);
+  }
+
+document.querySelector(".button").addEventListener("click", function() {
+  weather.search();
+});
+document.querySelector(".inputValue").addEventListener("keyup", function (event){
+  if(event.key == "Enter"){
+    weather.search();
+  }
 })
